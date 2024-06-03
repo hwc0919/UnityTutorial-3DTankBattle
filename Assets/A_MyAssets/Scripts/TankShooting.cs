401a97c2cc6fe61daceca709f10d33b5b0d0c8a5
He, Wanchen <hwc14@qq.com>
2024-06-03 16:12:14 +0800

    Add shooting audio.

--- Assets/A_MyAssets/Scripts/TankShooting.cs
+++ Assets/A_MyAssets/Scripts/TankShooting.cs
@@ -9,6 +9,10 @@ namespace MyTank
         public Rigidbody m_Shell;
         public Transform m_FireTransform;
         public Slider m_AimSlider;
+        public AudioSource m_shootingAudio;
+        public AudioClip m_ChargingClip;
+        public AudioClip m_FireClip;
+
         public float m_MinLaunchForce = 15f;        // The force given to the shell if the fire button is not held.
         public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
         public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
@@ -47,6 +51,10 @@ namespace MyTank
             {
                 m_Fired = false;
                 m_CurrentLaunchForce = m_MinLaunchForce;
+
+                // audio
+                m_shootingAudio.clip = m_ChargingClip;
+                m_shootingAudio.Play();
             }
             // fire button being held
             else if (Input.GetButton(m_FireButton) && !m_Fired)
@@ -68,6 +76,10 @@ namespace MyTank
             shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
             Debug.Log("gameObject: " + shellInstance.gameObject.ToString());
             m_CurrentLaunchForce = m_MinLaunchForce;
+
+            // audio
+            m_shootingAudio.clip = m_FireClip;
+            m_shootingAudio.Play();
         }
     }
 }
