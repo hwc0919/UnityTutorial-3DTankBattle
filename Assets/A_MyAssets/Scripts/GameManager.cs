e6adf2163f8407e4aa2f26fa4eb5f0e3af81a42a
He, Wanchen <hwc14@qq.com>
2024-06-04 18:45:13 +0800

    Camera movement.

--- Assets/A_MyAssets/Scripts/GameManager.cs
+++ Assets/A_MyAssets/Scripts/GameManager.cs
@@ -7,10 +7,27 @@ namespace MyTank
 {
     public class GameManager : MonoBehaviour
     {
+        public float m_StartDelay = 3f;
+        public float m_EndDelay = 3f;
+
+        public CameraControl m_CameraControl;
+
         public GameObject m_TankPrefab;
         public TankManager[] m_Tanks;
 
-        void Awake()
+        private WaitForSeconds m_StartWait;
+        private WaitForSeconds m_EndWait;
+
+        void Start()
+        {
+            m_StartWait = new WaitForSeconds(m_StartDelay);
+            m_EndWait = new WaitForSeconds(m_EndDelay);
+
+            SpawnAllTanks();
+            SetCameraTargets();
+        }
+
+        void SpawnAllTanks()
         {
             for (int i = 0; i < m_Tanks.Length; ++i)
             {
@@ -21,8 +38,15 @@ namespace MyTank
             }
         }
 
-        void Start()
+        void SetCameraTargets()
         {
+            Transform[] targets = new Transform[m_Tanks.Length];
+            for (int i = 0; i < targets.Length; ++i)
+            {
+                targets[i] = m_Tanks[i].m_TankInstance.transform;
+            }
+
+            m_CameraControl.m_Targets = targets;
         }
     }
 }
